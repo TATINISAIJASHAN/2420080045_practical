5a2074c6f0d45f1a6003e47e779f1ce44939c072
TATINISAIJASHAN <2420080045@klh.edu.in>
2025-08-05 10:43:18 +0530

    type casting

---
+++ autobox.java
@@ -0,0 +1,10 @@
+package myjavaproject;
+
+public class autobox {
+	public static void main(String[]args) {
+		float a=10.11f;
+		Float obj=a;
+		System.out.print("auto boxed: "+obj);
+	}
+
+}
---
+++ autocast.java
@@ -0,0 +1,11 @@
+package myjavaproject;
+
+public class autocast {
+	public static void main(String[] args) {
+		int myint=9;
+		double myDouble = myint;
+		System.out.println(myint);
+		System.out.println(myDouble);
+	}
+
+}
